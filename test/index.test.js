4e50054dd665730a01f54c93786af5d83af954ac
Abno Ribeiro <abno.ribeiro@wisereducacao.com>
2022-07-17 15:20:05 -0300

    feat: structure final testes cases

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,18 @@
-const pathFile = process.argv;
+import getFile from '../index';
 
-test('this is path is string?', () => {
-  expect(typeof pathFile).toBe('object');
-});
+describe("getFile::", ()=>{
+    it('this is path is a function?', ()=>{
+        expect(typeof getFile).toBe('function');
+    })
+    it('Result function get file is the object?', async ()=>{
+        const filePath = '/home/abno/Documentos/projects/alura/test/filesTests/texto1.md';
+        const result = await getFile(filePath)
+        expect(typeof result).toBe('object');
+    })
+    it('The return fuction if file dont have a link is a mensage "Não há links"?', async ()=>{
+        const filePath = '/home/abno/Documentos/projects/alura/test/filesTests/texto1_dontLinks.md';
+        const result = await getFile(filePath)
+        expect(result).toBe('Não há links')
+    })
+})
+
